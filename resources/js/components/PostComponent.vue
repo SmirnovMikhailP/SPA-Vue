540eccac12484fe27fdcd5ef8946cd986863dfe9
Mikhail Smirnov <SmirnovMikhailP@yandex.ru>
2023-02-26 14:53:44 +0300

    init vue_laravel

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <CreateComponent></CreateComponent>
+        <IndexComponent ref="index"></IndexComponent>
+<!--        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in personsAgeMoreTwenty" v-if="person.age > 20">
+                <th scope="row">{{ person.id }}</th>
+                <td>{{ person.name }}</td>
+                <td>{{ person.age }}</td>
+                <td>{{ person.job }}</td>
+            </tr>
+            </tbody>
+        </table>-->
+    </div>
+</template>
+
+<script>
+import CreateComponent from "./CreateComponent";
+import IndexComponent from "./IndexComponent";
+export default {
+    name: "PostComponent",
+
+    data(){
+        return {
+            persons: null
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods:{
+        parentLog(){
+
+        }
+    },
+
+    components: {
+        CreateComponent,
+        IndexComponent
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/SomeComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div>Colors: {{ obj.color }}</div>
+        <div>Numder: {{ obj.number > 10 ? 'more 10' : 'less 10'}}</div>
+        <div>Is published: {{ obj.isPublished ? 'Published' : 'Not Published'}}</div>
+    </div>
+</template>
+
+<script>
+import SinglePostComponent from "./SinglePostComponent";
+
+export default {
+    name: "SomeComponent",
+
+    data() {
+        return {}
+    },
+
+    props: [
+        'obj'
+    ],
+
+    mounted() {
+        this.$parent.$parent.parentLog()
+    },
+
+    methods: {}
+
+}
+</script>
+
+<style scoped>
+
+</style>
